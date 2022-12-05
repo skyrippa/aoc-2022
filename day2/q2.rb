5a6e5b46bafb31b0d9c0bbe3383850ebc6d3b8f7
skyrippa <iagob4599@gmail.com>
2022-12-05 09:24:40 -0300

    day 2 part 2

--- day2/q2.rb
+++ day2/q2.rb
@@ -1,13 +1,13 @@
 possible_outcomes = {
-  :"A X\n" => 4,
-  :"A Y\n" => 8,
-  :"A Z\n" => 3,
+  :"A X\n" => 3,
+  :"A Y\n" => 4,
+  :"A Z\n" => 8,
   :"B X\n" => 1,
   :"B Y\n" => 5,
   :"B Z\n" => 9,
-  :"C X\n" => 7,
-  :"C Y\n" => 2,
-  :"C Z\n" => 6,
+  :"C X\n" => 2,
+  :"C Y\n" => 6,
+  :"C Z\n" => 7,
 }
 my_score = 0
 
